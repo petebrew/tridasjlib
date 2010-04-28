7cbbafe73829bb7c50d8d8f568432637ce87c509
dmurphy10 <dmurphy10@843287a2-b704-49b0-8f52-e9cd02a0a56e>
2010-04-28 02:09:47 +0000

    Putting the JLib and FileIO projects under a central project (pom packaging in maven) where the JLib and IO are module projects of DendroLibrary.  This doesn't change anything from the original packages, this is purely an addition under a new project/folder

---
+++ DendroLibrary/TridasJLib/Libraries/MavenInstallCommands.sh
@@ -0,0 +1,5 @@
+mvn install:install-file -DgroupId=org.jvnet.jaxb2_commons.plugin.annotate -DartifactId=jaxb2-annotate -Dversion=0.5.1-SNAPSHOT -Dpackaging=jar -Dfile=jaxb2-basics-annotate-0.5.1.jar
+mvn install:install-file -DgroupId=com.sun.tools.xjc -DartifactId=collection-setter-injector -Dversion=0.1-SNAPSHOT -Dpackaging=jar -Dfile=collection-setter-injector.jar
+mvn install:install-file -DgroupId=org.jvnet.jaxb2_commons -DartifactId=xjc-if-ins -Dversion=0.5.3 -Dpackaging=jar -Dfile=xjc-if-ins+generics.jar
+mvn install:install-file -DgroupId=org.tridas.schema -DartifactId=tridasaandi -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=TridasAandI.jar
+mvn install:install-file -DgroupId=org.tridas.schema -DartifactId=tridas-annotations -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=TridasAnnotationsPlugin.jar
