be331c9847996cc0bc08d8d92f0dda4fa2a70631
Peter Brewer <p.brewer@ltrr.arizona.edu>
2011-06-15 21:19:29 +0000

    Moving Poms to point at new maven.tridas.org repository

--- Libraries/MavenInstallCommands.sh
+++ Libraries/MavenInstallCommands.sh
@@ -1,4 +1,7 @@
-#mvn install:install-file -DgroupId=org.jvnet.jaxb2_commons.plugin.annotate -DartifactId=jaxb2-basics-annotate      -Dversion=0.5.1  -Dpackaging=jar -Dfile=jaxb2-basics-annotate-0.5.1.jar
+# The Jars within this folder are those not found in standard Maven repositories.  They have been
+# placed in the maven.tridas.org repository but are included here in case this repository becomes
+# unavailable.  The jars can be installed with the following commands:
+
 mvn install:install-file -DgroupId=com.sun.tools.xjc                       -DartifactId=collection-setter-injector -Dversion=0.1    -Dpackaging=jar -Dfile=collection-setter-injector.jar
 mvn install:install-file -DgroupId=org.jvnet.jaxb2_commons                 -DartifactId=xjc-if-ins                 -Dversion=0.5.2  -Dpackaging=jar -Dfile=xjc-if-ins+generics.jar
 mvn install:install-file -DgroupId=org.tridas.schema                       -DartifactId=tridasaandi                -Dversion=1.0    -Dpackaging=jar -Dfile=TridasAandI.jar
